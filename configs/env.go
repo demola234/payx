1b2bedb5ca6dac08051093b81e273df862ac3f8d
Ademola <kolawoleoluwasegun567@gmail.com>
2022-09-13 19:49:04 +0100

    configfiles

--- configs/env.go
+++ configs/env.go
@@ -7,6 +7,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
+
 func MongoDBEnvUrl() string {
 	err := godotenv.Load()
 	if err != nil {
@@ -14,3 +15,4 @@ func MongoDBEnvUrl() string {
 	}
 	return os.Getenv("mongodb://localhost:27017")
 }
+
