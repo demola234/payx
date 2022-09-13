c7ebcf2db7f19d4651c9497baef987cb76f78670
Bolu <boluadetifa@gmail.com>
2022-09-13 23:13:08 +0100

    Fix: get all users

--- controllers/users.go
+++ controllers/users.go
@@ -10,9 +10,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"time"
+	"net/http"
 	"go-service/payx/helpers"
 	"go-service/payx/models"
-	"log"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
@@ -39,6 +39,7 @@ func GetUsers() gin.HandlerFunc {
 		startIndex, err = strconv.Atoi(c.Query("startIndex"))
 
 		matchStage := bson.D{{"$match", bson.D{{}}}}
+		groupStage := bson.D{{"$group", bson.D{{"_id", bson.D{{"_id", "null"}}}, {"total_count", bson.D{{"$sum", 1}}}, {"data", bson.D{{"$push", "$$ROOT"}}}}}}
 		projectStage := bson.D{
 			{"$project", bson.D{
 				{"_id", 0},
@@ -47,7 +48,7 @@ func GetUsers() gin.HandlerFunc {
 			
 		}}}
 
-		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{matchStage, projectStage})
+		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{matchStage, groupStage, projectStage})
 		defer cancel()
 		if err != nil{
 			c.JSON(500, gin.H{"status": "Failure",
@@ -57,7 +58,7 @@ func GetUsers() gin.HandlerFunc {
 		if err = result.All(ctx, &allUsers); err != nil{
 			log.Fatal(err)
 		}
-		c.JSON(200, gin.H{"status": "Success", "data": allUsers})
+		c.JSON(200, allUsers[0])
 	}
 }
 
